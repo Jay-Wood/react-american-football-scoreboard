b3b8c7bb32d118823438781792211a56184a85ef
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-15 14:21:48 -0700

    Reached MVP, cleaned up code formatting, working on stretch

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
-
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setCountHome] = useState(132);
   const [awayScore, setCountAway] = useState(311);
-
     return (
       <div className="container">
         <section className="scoreboard">
           <div className="topRow">
             <div className="home">
               <h2 className="home__name">Lions</h2>
-
               {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
               <div className="home__score">{homeScore}</div>
             </div>
             <div className="timer">00:03</div>
@@ -35,8 +31,8 @@ function App() {
             <button className="homeButtons__fieldGoal" onClick={ () => setCountHome(homeScore + 3)}>Home Field Goal</button>
           </div>
           <div className="awayButtons">
-            <button className="awayButtons__touchdown" onClick={ ()=> setCountAway(awayScore + 7)} >Away Touchdown</button>
-            <button className="awayButtons__fieldGoal" onClick={ () => setCountAway(awayScore + 3)} >Away Field Goal</button>
+            <button className="awayButtons__touchdown" onClick={ ()=> setCountAway(awayScore + 7)}>Away Touchdown</button>
+            <button className="awayButtons__fieldGoal" onClick={ () => setCountAway(awayScore + 3)}>Away Field Goal</button>
           </div>
         </section>
       </div>
